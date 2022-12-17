2ac55318135a0f747e0adae4865b53b4161f4bdd
UtkuMert <65029733+UtkuMert@users.noreply.github.com>
2022-12-17 14:32:15 +0300

    navbar design

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <div className='container'>
+        <div className="logo">logo</div>
+       <div className='categories'>
+          <Link className='link' to="/?cat=football">Football</Link>
+          <Link className='link' to="/?cat=cinema">Cinema</Link>
+          <Link className='link' to="/?cat=Technology">Technology</Link>
+          <span>Utku</span>
+          <span>Logout</span>
+          <span>
+            <Link to="/post">Post</Link>
+          </span>
+       </div>
+      </div>
+    </div>
   )
 }
 
