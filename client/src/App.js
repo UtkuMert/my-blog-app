5c755a0d7af1f49e2500e6a5c15e8cf391960aad
UtkuMert <65029733+UtkuMert@users.noreply.github.com>
2022-12-17 13:58:52 +0300

    pages routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,56 @@
-import './App.css';
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Login from "./pages/Login";
+import Post from "./pages/Post";
+import Register from "./pages/Register";
+import SinglePost from "./pages/SinglePost";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/singlePost/:id",
+          element: <SinglePost />,
+        },
+        {
+          path: "/post",
+          element: <Post />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+  ]);
   return (
-    <div className="App">
-      <p>test</p>
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
     </div>
   );
 }
