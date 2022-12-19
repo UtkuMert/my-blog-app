9b6d7ab7f9cc1eef3df9fa9c4717e4c4953b134f
UtkuMert <65029733+UtkuMert@users.noreply.github.com>
2022-12-19 18:06:49 +0300

    add post page design

--- client/src/pages/Post.jsx
+++ client/src/pages/Post.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 const Post = () => {
+  const [value, setValue] = useState("");
   return (
-    <div>Post</div>
-  )
-}
+    <div className="addPost">
+      <div className="content">
+        <input type="text" placeholder="Title" />
+        <div className="editContainer">
+          <ReactQuill
+            className="edit"
+            theme="snow"
+            value={value}
+            onChange={setValue}
+          />
+          ;
+        </div>
+      </div>
+      <div className="menu">
+        <div className="item">
+          <h1>Publish</h1>
+          <span>
+            <b>Status: </b> Draft
+          </span>
+          <span>
+            <b>Visibility: </b> Public
+          </span>
+          <input type="file" id="file" />
+          <div className="buttons">
+            <button>Save as a draft</button>
+            <button>Update</button>
+          </div>
+        </div>
+        <div className="item">
+          <h1>Categories</h1>
+          <div className="category">
+            <input type="radio" name="cat" value="football" id="football" />
+            <label htmlFor="football">Football</label>
+          </div>
+          <div className="category">
+            <input type="radio" name="cat" value="cinema" id="cinema" />
+            <label htmlFor="cinema">Cinema</label>
+          </div>
+          <div className="category">
+            <input type="radio" name="cat" value="cinema" id="cinema" />
+            <label htmlFor="technology">Technology</label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Post
+export default Post;
